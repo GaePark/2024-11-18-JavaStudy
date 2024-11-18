234e119ddb96001574be4de900a44c1bc569007a
GaePark <qkrwnsgus01@gmail.com>
2024-11-18 14:19:07 +0900

    commit

---
+++ FirstProject/src/첫번째자바.java
@@ -0,0 +1,76 @@
+/* 
+ 	1. 대문자 구분
+ 	2. 들여쓰기 => 가독성
+ 	3. 명령문 종료 반드시 ;
+ 		; => 파이썬 / Kotlin / javascript (;을 사용하지 않음)
+ 	4. 자바는 괄호가 옆에 붙어있음
+ 	5.문법
+ 	  데이터저장 ===> 데이터 처리(요청) ===> 화면 출력
+ 	     ㅣ               ㅣ             ㅣ
+ 	     			 연산자 / 제어문     명령프롬프트 : Application
+ 	     			                    문법
+ 	     			 ===========	 브라우저 출력 : Web Application
+ 	     			                    주력
+ 	     			 	 ㅣ			 모바일 출력 : Mobile Application
+ 	     			 	메소드		 spring : Java / Kotlin
+ 	   한개 저장 (메모리): 변수/상수
+ 	   여러개 저장 : 배열
+ 	              클래스
+ 	   --------------------------------------
+ 	   프로그램 종료시에 사라진다
+ 	   파일 / 오라클 => 영구적인 저장
+ 	   
+ 	   =======================> 기본문법
+ 	   1. 기능을 모아서 관리
+ 	   		===
+ 	   		 => 단락 : 메소드
+ 	   		 => 클랙스 / 객체 => 객체지향 프로그램
+ 	   		 => 프로그램 유지 (예외처리)
+ 	   		 =============================================> 8장까지의 내용
+ 	   		 => 라이브러리 : 자바에 지원
+ 	   		 => 1차 응용프로그램 제작
+ 	  =========================> Web
+ 	  1. HTML : 브라우저에서 실행
+ 	  			자바는 브라우저에서 일한 텍스트
+ 	  	
+ 	  	 => 투박 => CSS => JavaScfipt
+ 	  2. JSP (요청 => HTML => JAVA)
+ 	  		  응답 -> HTML -> JAVA => HTML
+ 	  	  ㅣ MCV
+ 	  	  	====
+ 	  	  	Front / Back
+ 	  	  	  ㅣ      ㅣ 자바/ 오라클 /Spring =============+ Full Stact
+ 	  	   JS / TS
+ 	 
+ 	 3. 통합 : Framework : Spring
+ 	 		  기본 틀 => 메이보드
+	 4. 데이터 분석 : Python / Numpy / Pandas
+	 			   Matplotlib
+	 5. NoSql => elasticsearch
+	 				ㅣ 오라클 역할
+	 6. 배포 : AWS
+	 ========================================================================
+	 Spring-Boot / JPA / MySQL / Reack-Query / Redux / vue3 / NextJS / TypeScript / NodeJS
+	 
+	 자바 / 오라클 / 알고리즘
+	 HTML /CSS / JavaScript / JSP
+	 Spring-Boot / Python / 데이터 분석
+	 elasticsearch
+	 ==============================================================
+	 Spring + MyBatis + JSP + JavaScript(Jquery)
+  */
+
+public class 첫번째자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello Java");
+		int age;
+		
+		age = 10;
+		System.out.println(age);
+	}
+
+}
